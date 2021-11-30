3ee30094da24a0969a1983c6ef4490b2572de383
xaraxx <sara.galvan.o91@gmail.com>
2021-11-30 10:28:46 -0500

    creating project routes

--- low_level_DB_driver/index.js
+++ low_level_DB_driver/index.js
@@ -35,6 +35,8 @@ app.get('/', (req, res) => {
   res.send('Hello World! This is an ORM test api')
 })
 
+app.use('/tables/', require('./src/tables.routes'))
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
